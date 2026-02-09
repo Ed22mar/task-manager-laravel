99c7c7d04447ad1f4fd62897205a86ab721c4368
Ed22mar <edgaricloud@gmail.com>
2026-02-09 18:34:46 +0100

    feat: Add authenticated manual CRUD routes for tasks

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,20 @@ Route::middleware('auth')->group(function () {
 });
 
 // Tasks Routes Resource
-/*Route::middleware('auth')->group(function () {
-    Route::resource('tasks', App\Http\Controllers\TaskController::class);
-});*/
 
-Route::middleware('auth')->get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
+Route::middleware('auth')->group(function (){
+    Route::get('/tasks',[TaskController::class,'index'])->name('tasks.index');
+    Route::get('/tasks/create', [TaskController::class,'create'])->name('tasks.create');
+
+    Route::post('/tasks',[TaskController::class,'store'])->name('tasks.store');
+
+    Route::get('/tasks/{task}',[TaskController::class,'show'])->name('tasks.show');
+
+    Route::get('/tasks/{task}/edit',[TaskController::class,'edit'])->name('tasks.edit');
+    Route::put('/tasks/{task}',[TaskController::class,'update'])->name('tasks.update');
+
+    Route::delete('/tasks/{task}',[TaskController::class,'destroy'])->name('tasks.destroy');
+
+});
 
 require __DIR__.'/auth.php';
